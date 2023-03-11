e4721b8b100052d11647e834030fe6dac5b5990a
Fola-flex <folarinolaore93@gmail.com>
2023-03-11 16:57:02 +0100

    Loading animation added

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import {  useState } from "react";
+import { useEffect, useState } from "react";
 import CardList from "../components/CardList";
 import SearchBox from '../components/SearchBox';
 import ButtonList from '../components/ButtonList'
@@ -6,71 +6,17 @@ import Scroll from '../components/Scroll';
 import './App.css';
 
 const App = (props) => {
-    const [robots] = useState([{
-        id: 1,
-        name: 'Leanne Graham',
-        username: 'Bret',
-        email: 'Sincere@april.biz'
-      },
-      {
-        id: 2,
-        name: 'Ervin Howell',
-        username: 'Antonette',
-        email: 'Shanna@melissa.tv'
-      },
-      {
-        id: 3,
-        name: 'Clementine Bauch',
-        username: 'Samantha',
-        email: 'Nathan@yesenia.net'
-      },
-      {
-        id: 4,
-        name: 'Patricia Lebsack',
-        username: 'Karianne',
-        email: 'Julianne.OConner@kory.org'
-      },
-      {
-        id: 5,
-        name: 'Chelsey Dietrich',
-        username: 'Kamren',
-        email: 'Lucio_Hettinger@annie.ca'
-      },
-      {
-        id: 6,
-        name: 'Mrs. Dennis Schulist',
-        username: 'Leopoldo_Corkery',
-        email: 'Karley_Dach@jasper.info'
-      },
-      {
-        id: 7,
-        name: 'Kurtis Weissnat',
-        username: 'Elwyn.Skiles',
-        email: 'Telly.Hoeger@billy.biz'
-      },
-      {
-        id: 8,
-        name: 'Nicholas Runolfsdottir V',
-        username: 'Maxime_Nienow',
-        email: 'Sherwood@rosamond.me'
-      },
-      {
-        id: 9,
-        name: 'Glenna Reichert',
-        username: 'Delphine',
-        email: 'Chaim_McDermott@dana.io'
-      },
-      {
-        id: 10,
-        name: 'Clementina DuBuque',
-        username: 'Moriah.Stanton',
-        email: 'Rey.Padberg@karina.biz'
-      }]);
+    const [robots, setRobots] = useState([]);
     const [searchfield, setSearchField] = useState("")
     const [loaded, setLoaded] = useState(false)
     const [robot_type, setRobotType] = useState("?200x200")
     
-    
+    useEffect(() => {
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(users => setRobots(users))
+    })
+
     const onSearchChange = (event) => {
         setSearchField(event.target.value)
     }
